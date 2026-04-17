2755e394b8f7451ecf434df38e3ccc3cea08fba4
Valarmathi <mathicsd2005@gmail.com>
2026-04-17 18:03:39 +0530

    updated api url for deployment

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8000',
+    baseURL: 'https://leagl-guard.onrender.com/api',
     headers: {
         'Content-Type': 'application/json',
     },
